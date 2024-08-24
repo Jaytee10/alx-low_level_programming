124cd1c76b01a69d1652d9c49352f4db863a2aba
jaytee10 <ehinmiduj@gmail.com>
2024-08-24 05:31:53 -0700

    101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.ph>
+#include <unistd.ph>
+/**
+ *  main - A c program that prints a line to standard error
+ *  Return 1 (Success)
+ */
+int main(void)
+{
+	fput("and that place of art is useful\" - Dora korpar, 2015-10-19\n", stdout);
+return (1);
+}
